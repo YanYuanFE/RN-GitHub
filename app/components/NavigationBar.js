1b9c198c7a01768ded4bb242aa998dbc4e602da1
yany2 <yuan.yan@dianrong.com>
2018-02-12 08:52:06 +0800

    navigation

---
+++ app/components/NavigationBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  View,
+  Text,
+  ListView
+} from 'react-native';
+
+export default class NavigationBar extends Component {
+  render() {
+    const { data } = this.props;
+    return <View>
+      <Text>{data.full_name}</Text>
+      <Text>{data.description}</Text>
+      <Text>{data.owner.avatar_url}</Text>
+      <Text>{data.stargazers_count}</Text>
+    </View>
+  }
+}
