ae3ef1105145075d86403ddc4f6ba807d95d6949
YanYuan <1025138284@qq.com>
2019-05-28 21:13:29 +0800

    feat: theme page done

--- src/components/ThemeCard.js
+++ src/components/ThemeCard.js
@@ -9,39 +9,41 @@ import {
 } from 'react-native';
 const screenW = Dimensions.get('window').width;
 const cols = 3;
-const cellWH = 100;
+const cellWH = 110;
 const vMargin = (screenW - cellWH * cols) / (cols + 1);
-const hMargin = 25;
+const hMargin = 10;
 
 export default class ThemeCard extends PureComponent {
     render() {
         const { data } = this.props;
         const item = data.data;
         return (
-            <View style={styles.wrapper}>
-                <View>
+            <View style={styles.container}>
+                <View style={styles.titleRow}>
                     <Text style={styles.themeTitle}>{data.title}</Text>
                 </View>
-                {
-                    Object.keys(item).map(key => {
-                        return (
-                            <View style={styles.themeItem} key={key}>
-                                <TouchableHighlight
+                <View style={styles.wrapper}>
+                    {
+                        Object.keys(item).map(key => {
+                            return (
+                              <View style={styles.themeItem} key={key}>
+                                  <TouchableHighlight
                                     key={key}
                                     style={[{backgroundColor: item[key]}, styles.themeBtn]}
                                     underlayColor='white'
                                     onPress={()=>this.onSelectTheme(key)}
-                                >
-                                    <View></View>
-                                </TouchableHighlight>
-                                <View>
-                                    <Text style={styles.themeText}>{key}</Text>
-                                    <Text style={styles.themeText}>{item[key]}</Text>
-                                </View>
-                            </View>
-                        )
-                    })
-                }
+                                  >
+                                      <View></View>
+                                  </TouchableHighlight>
+                                  <View>
+                                      <Text style={styles.themeText}>{key}</Text>
+                                      <Text style={styles.themeSubText}>{item[key]}</Text>
+                                  </View>
+                              </View>
+                            )
+                        })
+                    }
+                </View>
             </View>
         )
     }
@@ -51,6 +53,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'white',
+        paddingTop: 20
         // borderRadius: 3,
         // shadowColor: 'gray',
         // shadowOffset: {width: 2, height: 2},
@@ -64,31 +67,38 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flex: 1,
     },
+    titleRow: {
+        paddingLeft: 20,
+    },
     themeItem: {
-        borderRadius: 4,
+        flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'center',
         width: cellWH,
-        height: cellWH,
+        height: cellWH - 20,
         marginLeft: vMargin,
         marginTop: hMargin,
+    },
+    themeBtn: {
+        width: 50,
+        height: 50,
         shadowColor: 'gray',
         shadowOffset: {width: 2, height: 2},
         shadowOpacity: 0.5,
         shadowRadius: 2,
-    },
-    themeBtn: {
-        width: 50,
-        height: 50
+        borderRadius: 4,
+        marginRight: 10
     },
     themeText: {
+    color: '#2d3748',
+      fontSize: 14,
+},
+    themeSubText: {
         color: '#2d3748',
-        fontWeight: '500',
-        fontSize: 16,
+        fontSize: 12,
     },
     themeTitle: {
         color: '#1a202c',
         fontWeight: '500',
-        fontSize: 25,
+        fontSize: 20,
     }
-})
+});
