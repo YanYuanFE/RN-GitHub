d6c91f3213ab8878897a42640f34a04cc9bc61a2
YanYuan <1025138284@qq.com>
2022-04-09 16:16:09 +0800

    chore: clean

--- src/api/popular.js
+++
@@ -1,6 +0,0 @@
-import API from './api';
-import fetch from '../utils/fetch';
-
-export function fetchPopularRepo(repo) {
-  return fetch.get(API.GET_POPULAR_REPO(repo));
-}
--- src/context/themeContext.js
+++
@@ -1,9 +0,0 @@
-import React, {useContext, createContext} from 'react';
-
-export const ThemeContext = createContext({});
-
-export const ThemeProvider = ThemeContext.Provider;
-
-export const useTheme = () => {
-  return useContext(ThemeContext);
-};
